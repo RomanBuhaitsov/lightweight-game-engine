61131ba58e11f464ecb268b5e1de043f5e42baa9
RomanBuhaitsov <rbuhaitsov@gmail.com>
2023-04-15 13:58:49 +0200

    rebase develop

---
+++ src/audio_manager/audio_manager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <filesystem>
+#include <map>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
+class AudioManager {
+private:
+	std::map<std::string name, std::string path> sound_effects;
+	std::map<std::string name, std::string path> music;
+    bool music_playing, music_paused;
+public:
+    AudioManager(const std::string& dir = "src/static/sound");
+    void addMusicTrack(const std::string trackTitle, const std::filesystem::path &dir);
+    void addSoundEffect(const std::string sfxTitle, const std::filesystem::path &dir);
+    void playMusicTrack(const std::string trackTitle);
+    void playSoundEffect(const std::string sfxTitle);
+    void pauseTrack();
+};
