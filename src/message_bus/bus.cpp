49abcc17f92b86ddb0635dd96b9f47224aa5b3e7
Norbert Wyszkowski <n.wyszkowski@cthings.co>
2023-05-29 17:41:40 +0200

    remove reference from message bus notify loop

--- src/message_bus/bus.cpp
+++ src/message_bus/bus.cpp
@@ -1,35 +1,31 @@
 #include "bus.h"
 #include "message.h"
 
+#include "node.h"
 #include <functional>
 #include <queue>
 #include <vector>
-#include "node.h"
 
-void MessageBus::addReceiver(BusNode* messageReceiver)
-{
+void MessageBus::addReceiver(BusNode *messageReceiver) {
   receivers.push_back(messageReceiver);
 }
 
-void MessageBus::sendMessage(const Message & message) { messages.push(message); }
+void MessageBus::sendMessage(const Message &message) { messages.push(message); }
 
-void MessageBus::notify()
-{
-  while (!messages.empty())
-  {
-    for (auto & iter = receivers.begin(); iter != receivers.end(); iter++)
-    {
-        (**iter)(messages.front());
+void MessageBus::notify() {
+  while (!messages.empty()) {
+    for (auto iter = receivers.begin(); iter != receivers.end(); iter++) {
+      (**iter)(messages.front());
     }
     messages.pop();
   }
 }
 
-void MessageBus::removeReceiver(BusNode* messageReceiver) {
-    for (auto& it = receivers.begin(); it != receivers.end(); ++it) {
-        if (*it == messageReceiver) {
-            receivers.erase(it);
-            break;
-        }
+void MessageBus::removeReceiver(BusNode *messageReceiver) {
+  for (auto it = receivers.begin(); it != receivers.end(); ++it) {
+    if (*it == messageReceiver) {
+      receivers.erase(it);
+      break;
     }
+  }
 }
